2aa6c1df464288827802da778291efb59a1f8b12
Meghana-VS <meghavs7022@gmail.com>
2025-07-04 20:36:34 +0530

    Move zeroes to the end

--- src/two_pointers/MoveZeroes.java
+++ src/two_pointers/MoveZeroes.java
@@ -6,22 +6,35 @@ import java.util.List;
 
 public class MoveZeroes {
     public static void main(String[] args) {
-        int[] nums = {0};
+        int[] nums = {0,1,0,3,12};
         moveZeroes(nums);
     }
     public static void moveZeroes(int[] nums) {
-//**************************(TC - o(n), SC - o(n))************************
+//**************************(TC - o(n))************************
         int n = nums.length;
-        List<Integer> list = new ArrayList<>();
-        for(int num : nums){
-            if(num != 0) list.add(num);
-        }
-        for(int i=0; i<list.size(); i++){
-            nums[i] = list.get(i);
-        }
-        for(int i=list.size(); i<n; i++){
-            nums[i] = 0;
+        int non_zero = 0;
+        for(int i=0; i<n; i++){
+            if(nums[i] != 0){
+                int temp = nums[i];
+                nums[i] = nums[non_zero];
+                nums[non_zero] = temp;
+                non_zero++;
+            }
         }
         System.out.println(Arrays.toString(nums));
+
+//**************************(TC - o(n), SC - o(n))************************
+//        List<Integer> list = new ArrayList<>();
+//        for(int num : nums){
+//            if(num != 0) list.add(num);
+//        }
+//        for(int i=0; i<list.size(); i++){
+//            nums[i] = list.get(i);
+//        }
+//        for(int i=list.size(); i<n; i++){
+//            nums[i] = 0;
+//        }
+//        System.out.println(Arrays.toString(nums));
+//    }
     }
 }
