2e6f5afa23b7e3b3e3929d79fdb7c108244b0c5f
Meghana-VS <meghavs7022@gmail.com>
2025-07-25 21:36:44 +0530

    maximum consecutive ones

--- src/sliding_window/Max_Consecutive_Ones_III.java
+++ src/sliding_window/Max_Consecutive_Ones_III.java
@@ -4,20 +4,40 @@ public class Max_Consecutive_Ones_III {
     public static void main(String[] args) {
         int[] nums = {1,1,1,0,0,0,1,1,1,1,0};
         int k = 2;
+//        int[] nums = {1,1,1,0};
+//        int k = 1;
         System.out.println(longestOnes(nums, k));
     }
 
     public static int longestOnes(int[] nums, int k) {
-        int i = 0;
-        int j = 0;
-        while(i < nums.length){
-            if(nums[i] == 0) k--;
-            if(k < 0){
-                if(nums[j] == 0) k++;
-                j++;
+//***********************(TC - o(n), SC - o(1))*************************
+//        int i = 0;
+//        int j = 0;
+//        while(i < nums.length){
+//            if(nums[i] == 0) k--;
+//            if(k < 0){
+//                if(nums[j] == 0) k++;
+//                j++;
+//            }
+//            i++;
+//        }
+//        return i-j;
+
+//**********************************************************************
+        int start = 0;
+        int max_length = 0;
+        int zero_count = 0;
+
+        for(int end = 0; end < nums.length; end++){
+            if(nums[end] == 0) zero_count++;
+            if(zero_count > k){
+                if(nums[start] == 0){
+                    zero_count--;
+                }
+                start++;
             }
-            i++;
+            max_length = Math.max(max_length, end-start+1);
         }
-        return i-j;
+        return  max_length;
     }
 }
